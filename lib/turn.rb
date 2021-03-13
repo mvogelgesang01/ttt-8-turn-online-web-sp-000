61bd564c5748ee344e5fcacb10fc0675b8370c25
Melissa Vogelgesang <mvogelgesang01@gmail.com>
2021-03-13 21:43:20 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,27 @@
+def display_board (board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  input.to_i-1
+end
+
+def move(board, index, player="X")
+  #update_array_at_with(array, index, value)
+    board[index] = player
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  valid_move?(board, index)
+end
+
 def valid_move?(board, position)
   if !position_taken(board, position) && position.between?(0,8)
     true
@@ -18,3 +42,4 @@ def position_taken(board, position)
     true
 
   end
+end
